825946e58aa335d69a4309dd0aba267ce94e3ae9
jiajinshuo <1948369431@qq.com>
2020-03-29 22:57:00 +0800

    0329

---
+++ src/main/java/com/qlu/springbtjdbc/SpringbtJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.qlu.springbtjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbtJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbtJdbcApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qlu/springbtjdbc/config/DruidConfig.java
@@ -0,0 +1,63 @@
+package com.qlu.springbtjdbc.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jiajinshuo
+ * @create 2020-02-11 15:48
+ * 使yml配置文件生效
+ */
+@Configuration
+public class DruidConfig {
+    //把自己配置的和Bean绑定起来，使配置文件生效
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+    //配置druid的监控
+    //配置一个管理后台的servlet。注册一个servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String,String> initParams = new HashMap<>();
+
+        initParams.put("loginUsername","admin");
+        initParams.put("loginPassword","123456");
+        initParams.put("allow","");//默认就是允许所有访问
+        initParams.put("deny","192.168.15.21");
+
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+
+
+    //2、配置一个web监控的filter
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+
+        Map<String,String> initParams = new HashMap<>();
+        //排除哪些请求
+        initParams.put("exclusions","*.js,*.css,/druid/*");
+
+        bean.setInitParameters(initParams);
+
+        bean.setUrlPatterns(Arrays.asList("/*"));
+
+        return  bean;
+    }
+}
